6d30ad9826080556a8b8c40a81f3cd860bf41f3b
Radim Kašpárek <radim248@gmail.com>
2019-07-23 14:42:01 +0100

    one piece of logging used for debugging removed

--- src/main.py
+++ src/main.py
@@ -97,8 +97,6 @@ def get_n_export_one_report(api_token, app_id, report_name, from_date, to_date):
             out_file.write('\n')
 
     # returns the colnames of the file
-    logging.info('The lenght of the first line is: ', str(len(
-        bytes_data.decode("utf-8").splitlines())))
     return str(bytes_data.decode("utf-8").splitlines()[0]).split(',')
 
 
